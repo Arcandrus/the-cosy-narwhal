a576142d768e1bd29beeed67c6c1660c57c7e240
Arcandrus <eric.harper87@gmail.com>
2025-06-30 12:27:05 +0100

    Began project the_cosy_narwharl

---
+++ the_cosy_narwharl/the_cosy_narwharl/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for the_cosy_narwharl project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_cosy_narwharl.settings')
+
+application = get_asgi_application()
