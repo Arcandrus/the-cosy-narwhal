9667acb736bb611e09d6bf0bbf50eb64526840b8
Arcandrus <eric.harper87@gmail.com>
2025-07-21 14:28:47 +0100

    Settings adjustments

--- the_cosy_narwhal/settings.py
+++ the_cosy_narwhal/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
-import stripe
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
