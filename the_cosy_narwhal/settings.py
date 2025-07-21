7dd367f6f0c6100ea058e793e5f2a193e28657f1
Arcandrus <eric.harper87@gmail.com>
2025-07-21 18:10:57 +0100

    Changes to AWS

--- the_cosy_narwhal/settings.py
+++ the_cosy_narwhal/settings.py
@@ -204,4 +204,4 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Your media URL will point to the S3 bucket domain
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}media/'
